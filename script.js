79a94715c95f4fb50cfdeaecffdcb6fa0db76f69
Andy Sterkowitz <apsterks@gmail.com>
2018-11-06 19:27:39 -0600

    Adds event listeners

--- script.js
+++ script.js
@@ -6,7 +6,36 @@ window.onload = function() {
     let picture5 = "frog";
     let picture6 = "elephant";
 
-    var pictureContainer
+    let image1 = document.getElementById('image-1');
+    let image2 = document.getElementById('image-2');
+    let image3 = document.getElementById('image-3');
+    let image4 = document.getElementById('image-4');
+    let image5 = document.getElementById('image-5');
+    let image6 = document.getElementById('image-6');
+
+    image1.addEventListener('click', function(){
+
+    });
+
+    image2.addEventListener('click', function(){
+
+    });
+
+    image3.addEventListener('click', function(){
+
+    });
+
+    image4.addEventListener('click', function(){
+
+    });
+
+    image5.addEventListener('click', function(){
+
+    });
+
+    image6.addEventListener('click', function(){
+
+    });
 
     // add event listeners
 };
