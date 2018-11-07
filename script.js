c40ff25c906d505a3b9e22e83677bdc2d3a52995
Andy Sterkowitz <apsterks@gmail.com>
2018-11-06 19:19:40 -0600

    Add stylesheet

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 window.onload = function() {
-    // load images
-    let image1 =
+    let picture1 = "monkey";
+    let picture2 = "elephant";
+    let picture3 = "monkey";
+    let picture4 = "frog";
+    let picture5 = "frog";
+    let picture6 = "elephant";
+
+    var pictureContainer
 
     // add event listeners
 };
